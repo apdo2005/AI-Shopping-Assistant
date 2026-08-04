fd1e39d6bdece3cac87f88e887f46c2cfd95c79b
apdo2005 <apdo07751@gmail.com>
2026-08-05 02:35:18 +0300

    three important color added into app_colors.dart

---
+++ ai_shopping_assistant/lib/core/constants/app_colors.dart
@@ -0,0 +1,53 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primary = Color(0xff007AFF);
+  static const secondary = Color(0xff8E2DE2);
+  static const tertiary = Color(0xff4A00E0);
+  static const netural = Color(0xffF5F5F7);
+  static const background = Color(0xFFF5F7FA);
+  static const surface = Color(0xFFFFFFFF);
+  static const textPrimary = Color(0xFF1B243F);
+  static const textSecondary = Color(0xFF7C85A2);
+  static const textHint = Color(0xFFBCC1D2);
+  static const divider = Color(0xFFE1E5F2);
+
+  static const red = Color(0xFFFF4D4F);
+
+  static const green = Color(0xFF52C41A);
+  static const yellow = Color(0xFFFAAD14);
+  static const gray = Color(0xFFBFBFBF);
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+  static const blue = Color(0xFF0062FF);
+  static const purple = Color(0xFF8B5CF6);
+  static const orange = Color(0xFFFF9900);
+  static const pink = Color(0xFFEC4899);
+  static const indigo = Color(0xFF6366F1);
+  static const teal = Color(0xFF14B8A6);
+  static const cyan = Color(0xFF06B6D4);
+  static const lightBlue = Color(0xFF93C5FD);
+  static const lightGray = Color(0xFFf0f0f0);
+  static const shadow = Color(0xFF000000);
+  static const List<Color> chartColors = [
+    primary,
+    secondary,
+    red,
+    green,
+    yellow,
+    gray,
+    black,
+    white,
+    blue,
+    purple,
+    orange,
+    pink,
+    indigo,
+    teal,
+    cyan,
+    lightBlue,
+    lightGray,
+  ];
+  static const error = Color(0xFFf44336);
+  static const greendark = Color(0xFF4CAF50);
+}
